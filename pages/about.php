c5b848d5d8eb1668791d8c6d49469ab9286f284a
Linda Vue <lvue@ncsu.edu>
2015-09-30 20:37:15 -0400

    Initial layout/template set up. Content should now be okay to be added.

---
+++ pages/about.php
@@ -0,0 +1,9 @@
+<?php include 'layout/header.html';?>
+
+  <div class="container">
+		<div class="content">
+				HELLO WORLD!asdfasdf
+				<?php echo 'this is php';?>
+		</div>
+
+<?php include 'layout/footer.html';?>
