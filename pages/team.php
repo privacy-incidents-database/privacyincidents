a426092e0740d1289e69173c5a40fd1b14749824
Linda Vue <lvue@engr-ras-201.eos.ncsu.edu>
2015-10-20 20:59:59 -0400

    Add images to contact page

--- pages/team.php
+++ pages/team.php
@@ -1,20 +1,24 @@
 <?php include 'layout/header.html';?>
 
   <div class="container team">	
-		
 		<div id="team-info">
 			<h2>About the team </h2>
 			<p> We are based from <a href="http://www.ncsu.edu">North Carolina State University</a> in Raleigh, NC.</p>
 		</div>	
 		<div class="row">	
 			<div class="bio col-md-4">
-				<div class="circle"></div>
+				<!--<div class="circle"></div>-->
+				<div class="img">
+					<img src="/privacyincidents/assets/imgs/jnstaddo.png">
+				</div>
 				<h4>Dr. Jessica Staddon</h4>
 				<p>Project Owner</p>
 			</div>
 				
 			<div class="bio col-md-4">
-				<div class="circle"></div>
+				<div class="img">
+					<img src="/privacyincidents/assets/imgs/lvue.png">
+				</div>
 				<h4>Linda Vue</h4>
 				<p>Research Associate</p>
 			</div>
@@ -24,6 +28,5 @@
 				<h4>Yuxu Yang</h4>
 				<p>Research Associate</p>
 			</div>	
-		</div>d		
-
+		</div>	
 <?php include 'layout/footer.html';?>
