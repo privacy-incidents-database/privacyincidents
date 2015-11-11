2ce7ca162b41263aff10f87d3ff7c37aed904839
yyang21 <yyang21@ncsu.edu>
2015-11-11 15:36:13 -0500

    add current time

--- pages/actions.php
+++ pages/actions.php
@@ -5,7 +5,8 @@
 	include 'db.php';
 	@mysql_connect(host,user,pw) or die('Could not connect to MySQL database. ');
 	mysql_select_db(database);
-
+		date_default_timezone_set('NYC');
+		$today = date("Y-m-d");
 	    $description = $_POST['description'];
 	    $PublicLink = $_POST['PublicLink'];
 	    $occurred_date = $_POST['Occurred_Date'];
@@ -22,7 +23,7 @@
 				echo '<td>' . $contributor_name. '</td>';
 				echo '<td>' . $contributor_email. '</td>';
 		echo '</tr>';
-	    $query = mysql_query("INSERT INTO `Privacy incidents` (`Descr`,`link`,`date_occurred`,`who_company`,`contributor_name`,`Contributor_email`) VALUES ('$description','$PublicLink','$occurred_date','$company','$contributor_name','$contributor_email')");
+	    $query = mysql_query("INSERT INTO `Privacy incidents` (`date_submitted`,`Descr`,`link`,`date_occurred`,`who_company`,`contributor_name`,`Contributor_email`) VALUES ('$today','$description','$PublicLink','$occurred_date','$company','$contributor_name','$contributor_email')");
 	    if (mysql_num_rows($query) != 0) {
 		echo 'INSERT FAILURE!'; 
 		die();
