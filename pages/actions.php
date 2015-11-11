bfe6d1da7bb8a23b19d07aa361dae1dde3f87f53
yyang21 <yyang21@ncsu.edu>
2015-11-11 14:49:06 -0500

    more fields.

--- pages/actions.php
+++ pages/actions.php
@@ -8,13 +8,19 @@
 
 	    $description = $_POST['description'];
 	    $PublicLink = $_POST['PublicLink'];
+	    $Occurred_Date = $_POST['Occurred_Date'];
+	    $Company = $_POST['which_company'];
+	    $Contributor_Name = $_POST['Contributor_Name'];
+	    $Contributor_Email = $_POST['Contributor_Email'];
 	   /* echo '<tr>';
 				echo '<td>' . $description . '</td>';
 	
 				echo '<td>' . $PublicLink. '</td>';
 	
 		echo '</tr>';*/
-	    $query = mysql_query("INSERT INTO `Privacy incidents` (`Descr`,`link`) VALUES ('$description','$PublicLink')");
+	    $query = mysql_query("INSERT INTO `Privacy incidents` (`Descr`,`link`,`date_occurred`, 
+	    	`who_company`,`contributor_name`,`Contributor_email`) VALUES ('$description','$PublicLink',
+	    	'Occurred_Date','Company','Contributor_Name','Contributor_Email')");
 	    if (mysql_num_rows($query) != 0) {
 		echo 'INSERT FAILURE!'; 
 		die();
