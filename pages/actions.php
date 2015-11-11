81efd3873ea68adbb14f434cc6de2fae5a09dd07
yyang21 <yyang21@ncsu.edu>
2015-11-11 12:41:40 -0500

    fix the extra contribute.html

--- pages/actions.php
+++ pages/actions.php
@@ -4,10 +4,15 @@
 	mysql_select_db(database);
 
 ?>
-<?php include 'layout/contribute.html';?>
 <?php
 	    $description = $_POST['description'];
 	    $PublicLink = $_POST['PublicLink'];
+	    echo '<tr>';
+				echo '<td>' . $description . '</td>';
+	
+				echo '<td>' . $PublicLink. '</td>';
+	
+		echo '</tr>';
 	    $query = mysql_iquery("INSERT INTO `Privacy incidents` ('Descr',`link`) VALUES ('$description','$PublicLink')");
 	    if (mysql_num_rows($query) == 0) {
 		echo 'INSERT FAILURE!'; 
