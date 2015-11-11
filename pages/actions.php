6a76e0e00f5f7dd763e65da8928680abc1b6991c
yyang21 <yyang21@ncsu.edu>
2015-11-11 13:17:42 -0500

    checking the insert failure

--- pages/actions.php
+++ pages/actions.php
@@ -5,15 +5,16 @@
 
 	    $description = $_POST['description'];
 	    $PublicLink = $_POST['PublicLink'];
-	    echo '<tr>';
+	   /* echo '<tr>';
 				echo '<td>' . $description . '</td>';
 	
 				echo '<td>' . $PublicLink. '</td>';
 	
-		echo '</tr>';
-	    $query = mysql_query("INSERT INTO `Privacy incidents` (`Descr`,`link`) VALUES ('$description','$PublicLink')");
-	    if (mysql_num_rows($query) == 0) {
-		echo 'INSERT FAILURE!'; 
+		echo '</tr>';*/
+		$query = "INSERT INTO `Privacy incidents` (`Descr`,`link`) VALUES ('$description','$PublicLink')"
+	    $result = mysql_query($query);
+	    if (!$result) {
+		   echo 'INSERT FAILURE!'; 
 		die();
 	}
 ?>
