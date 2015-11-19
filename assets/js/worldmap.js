0f284fdbc6bd5bb1d123e678babed6821ae4f457
Linda Vue <lvue@ncsu.edu>
2015-11-18 20:47:49 -0500

    Working?

---
+++ assets/js/worldmap.js
@@ -0,0 +1,20 @@
+var format = function(d) {
+    // d = d / 1000000;
+    // return d3.format(',.02f')(d) + 'M';
+    return d3.format(',.0f')(d) + ' incidents';
+}
+
+var map = d3.geomap.choropleth()
+    .geofile('../assets/d3-geomap/topojson/world/countries.json')
+    .colors(colorbrewer.Reds[9])
+    .column('Incident Count')
+    .format(format)
+    .legend(true)
+    .unitId('Country Code')
+    .width("1250");
+
+d3.csv('../assets/d3-geomap/data/incident_counts.csv', function(error, data) {
+    d3.select('#map')
+        .datum(data)
+        .call(map.draw, map); 
+});
---
+++ pages/analytics.php
@@ -0,0 +1,18 @@
+<?php
+	include 'layout/header.html';
+?>
+        <link href="../assets/d3-geomap/css/d3.geomap.css" rel="stylesheet">
+
+        <script src="../assets/d3-geomap/vendor/d3.geomap.dependencies.min.js"></script>
+        <script src="../assets/d3-geomap/js/d3.geomap.min.js"></script>
+        <script type="text/javascript" src="../assets/js/worldmap.js"></script>
+
+<div class="container container-fluid">
+	<div>
+		<h2>Incident Counts Around the World</h2>
+	</div>
+	<div id="map">
+    </div>
+</div>
+
+<?php include 'layout/footer.html';?>
