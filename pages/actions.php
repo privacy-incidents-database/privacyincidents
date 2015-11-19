a6f54d1622791072398fd11d968ac18f74989688
yyang21 <yyang21@ncsu.edu>
2015-11-18 21:31:55 -0500

    yangyuxu

--- pages/actions.php
+++ pages/actions.php
@@ -10,13 +10,24 @@
 	    $description = $_POST['description'];
 	    $PublicLink = $_POST['PublicLink'];
 	    $occurred_date = $_POST['Occurred_Date'];
-	    $companyone = implode(',#', $_POST['which_company']);
-	    if(isset($_POST['which_company_checkbox'])){
-	    	$companyThree = $_POST['which_company_custom'];
-	    	$companyTwo = ",#";
-	    	$company= $companyone.$companyTwo.$companyThree;
+	    if(isset($_POST['which_company'])){
+	    	$sy = "#"
+	    	$companyOriginal = implode(',#', $_POST['which_company']);
+	    	$companyone = $sy.$companyOriginal;
+	    	if(isset($_POST['which_company_checkbox'])){
+	    		$companyThree = $_POST['which_company_custom'];
+	    		$companyTwo = ",#";
+	    		$company= $companyone.$companyTwo.$companyThree;
+	    	} else {
+	    		$company= $companyone;
+	    	}
 	    } else {
-	    	$company= $companyone;
+	    	if(isset($_POST['which_company_checkbox'])){
+	    		$companyThree = $_POST['which_company_custom'];
+	    		$companyTwo = "#";
+	    		$company= $companyTwo.$companyThree;
+	    	} else {
+	    		$company= "";
 	    }
 	    $who_role = implode(',#', $_POST['which_role']);
 	    $contributor_name = $_POST['Contributor_Name'];
@@ -31,7 +42,7 @@
 				echo '<td>' . $contributor_email. '</td>';
 		echo '</tr>';
 	    $query = mysql_query("INSERT INTO `Privacy incidents` (`date_submitted`,`Descr`,`link`,`date_occurred`,`who_company`,`contributor_name`,`Contributor_email`,`who_role`) 
-	    	VALUES ('$today','$description','$PublicLink','$occurred_date','#$company','$contributor_name','$contributor_email','#$who_role')");
+	    	VALUES ('$today','$description','$PublicLink','$occurred_date','$company','$contributor_name','$contributor_email','$who_role')");
 	    if (mysql_num_rows($query) != 0) {
 		echo 'INSERT FAILURE!'; 
 		die();
