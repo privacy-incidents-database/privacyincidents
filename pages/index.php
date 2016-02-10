e8803dabec92304409df02b70416546e81e8687a
yyang21 <yyang21@ncsu.edu>
2016-02-10 13:59:58 -0500

    fix pages/index.php

--- pages/index.php
+++ pages/index.php
@@ -1,7 +1,7 @@
 <?php include 'layout/header.html';?>
 
   <!-- Header Carousel -->
-  <header id="myCarousel" class="carousel slide">
+  <header id="myCarousel" class="carousel slide" data-interval="false">
       <!-- Indicators -->
       <ol class="carousel-indicators">
           <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
