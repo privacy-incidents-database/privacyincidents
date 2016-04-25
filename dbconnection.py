c16f8361c15b5a7d121ae2f6a3981cb53aa242a1
ksant <ksant@ncsu.edu>
2016-04-25 13:10:06 -0400

    Update dbconnection.py

--- dbconnection.py
+++ dbconnection.py
@@ -16,24 +16,21 @@
 		"review": 0 
 ]'''
 
-import mysql
+
+import mysql.connector
+import json
 
 class DBconnection:
 
-	def __init__(self,database,host,user,pwd):
-		self.database = database
-		self.user = user
-		self.pwd = pwd
-		self.host = host
+	def __init__(self,config):
+		self.config = config
 
-	def insert(self,jsonfile=None):
+	def insert(self,jsonfile):
 		''' insert data into DB from json objects in file
 		'''
 
 		try:
-			self.cnx = mysql.connector.connect(user=self.user, password=self.pwd,
-                              host=self.host,
-                              database=self.database)
+			self.cnx = mysql.connector.connect(**self.config)
 		except:
 			print "Error connecting to DB"
 
@@ -45,40 +42,51 @@ class DBconnection:
 			for row in input_json:
 				self.inputrow = {}
 				
-				query = ("SELECT locationID FROM location WHERE location={}".format(row["location"]))
+				query = ("SELECT locationID FROM location WHERE location='{}'".format(row["location"]))
 				self.cursor.execute(query)
 				for each in self.cursor:
 					self.inputrow["locationID"] = each[0]
 				
-				query = ("SELECT companyID FROM company WHERE company in {}".format(row["company"]))
+				company = [c.encode("ascii") for c in row["company"]]
+				self.inputrow["companyID"] = []
+				query = ("SELECT companyID FROM company WHERE company in {}".format(tuple(company)))
 				self.cursor.execute(query)
 				for each in self.cursor:
-					self.inputrow["companyID"].append(each)
+					self.inputrow["companyID"].append(each[0])
 			
-				query = ("SELECT incidenttypeID FROM incidenttype WHERE type in {}".format(row["incidenttype"]))
+				incidenttype = [c.encode("ascii") for c in row["incidenttype"]]
+				self.inputrow["incidenttypeID"] = []
+				query = ("SELECT incidenttypeID FROM incidenttype WHERE type in {}".format(tuple(incidenttype)))
 				self.cursor.execute(query)
 				for each in self.cursor:
-					self.inputrow["incidenttypeID"].append(each)
+					self.inputrow["incidenttypeID"].append(each[0])
 				
-				query = ("SELECT rootcauseID FROM rootcause WHERE rootcause in {}".format(row["rootcause"]))
+				rootcause = [c.encode("ascii") for c in row["rootcause"]]
+				self.inputrow["rootcauseID"] = []
+				query = ("SELECT rootcauseID FROM rootcause WHERE rootcause in {}".format(tuple(rootcause)))
 				self.cursor.execute(query)
 				for each in self.cursor:
-					self.inputrow["rootcauseID"].append(each)
+					self.inputrow["rootcauseID"].append(each[0])
 
-				query = ("SELECT peopleID FROM people WHERE peopleaffected in {}".format(row["people"]))
+				people = [c.encode("ascii") for c in row["people"]]
+				self.inputrow["peopleID"] = []
+				query = ("SELECT peopleID FROM people WHERE peopleaffected in {}".format(tuple(people)))
 				self.cursor.execute(query)
 				for each in self.cursor:
-					self.inputrow["peopleID"].append(each)
+					self.inputrow["peopleID"].append(each[0])
 
-				insertquery = ("INSERT INTO incidentdetails VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)")
-				incidentdata = (row["date_submitted"],row["date_occurred"],row["contributor_name"],row["contributor_email"],row["description"],\
+				print(self.inputrow)
+				self.inputrow["incidentID"] = []
+				insertquery = ("INSERT INTO incidentdetails 	(date_submitted,date_occured,casestudy,contributor_name,contributor_email,description,link,source,locationID,review) "
+												" VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
+				incidentdata = (row["date_submitted"],row["date_occurred"],"",row["contributor_name"],row["contributor_email"],row["description"],\
 						row["url"],row["source"],self.inputrow["locationID"],row["review"])
 				self.cursor.execute(insertquery,incidentdata)
 				self.inputrow["incidentID"] = self.cursor.lastrowid
 
 				insertquery = ("""INSERT INTO incidents VALUES (%s,%s,%s,%s,%s)""")	
 				for company in self.inputrow["companyID"]:
-					for incidenttype in self.inputrow["incidenttype"]:
+					for incidenttype in self.inputrow["incidenttypeID"]:
 						for rootcause in self.inputrow["rootcauseID"]:
 							for people in self.inputrow["peopleID"]:
 								data = (self.inputrow["incidentID"],incidenttype,company,people,rootcause)
@@ -91,13 +99,13 @@ class DBconnection:
 		return incidentIDs
 
 
-	def queryall(self):
+	def queryallincidents(self):
 		'''returns all incidents from DB'''
 		pass
 
 
 
-	def queryincidents(self,incidentID):
+	def queryincident(self,incidentID):
 		'''returns json object of data for given incidentID'''
 
 		try:
@@ -158,3 +166,18 @@ class DBconnection:
 		self.cursor.close()
 		self.cnx.close()
 		return json.dumps(self.outputrow,ensure_acsii=False)
+
+if __name__ == "__main__":
+		config = {
+  	'user': 'user',
+  	'password': 'passwd',
+  	'host': '127.0.0.1',
+		'database':'Privacyincidents'
+		}
+
+		db = DBconnection(config)
+	
+		incidentids = db.insert("inputdata.json")
+		for ID in incidentids:
+				db.queryincident(ID)
+
