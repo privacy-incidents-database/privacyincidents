828a01a4121b60ec1cae3e07c83da0a5e69cf6e7
staddon <jessica.staddon@gmail.com>
2016-06-29 14:02:05 -0400

    Update team.php

--- pages/team.php
+++ pages/team.php
@@ -6,7 +6,7 @@
 			<p> We are based at <a href="http://www.ncsu.edu">North Carolina State University</a>, UNC Charlotte, Clemson University and Rochester Institute of Technology</p>
 		</div>
 		<div class="content ol-md-4">
-			<p class="rteleft"><strong>Leads</strong></p>: <a href="http://home.jessicastaddon.com/">Jessica Staddon</a>, <a href="http://hci.uncc.edu/~richter/">Heather Richter Lipford</a>, <a href="http://www.usabart.nl/portfolio/#home.html">Bart Knijnenburg</a> and <a href="http://www.se.rit.edu/~pkm/">Pradeep Kumar Murukannaiah</a></p>
+			<p class="rteleft"><strong>Leads</strong></p> <a href="http://home.jessicastaddon.com/">Jessica Staddon</a>, <a href="http://hci.uncc.edu/~richter/">Heather Richter Lipford</a>, <a href="http://www.usabart.nl/portfolio/#home.html">Bart Knijnenburg</a> and <a href="http://www.se.rit.edu/~pkm/">Pradeep Kumar Murukannaiah</a></p>
     		<p class="rteleft">NCSU:</p>
 			<p class="rteleft"><strong>Faculty Members:</strong></p>
 				<ul>
