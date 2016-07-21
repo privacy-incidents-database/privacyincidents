69d25a6ae45ab0dc7a6a025bdb1a8b6b2285af0b
eshasharma <eshasharma@users.noreply.github.com>
2016-07-21 18:06:22 -0400

    Update incidents.php

--- pages/incidents.php
+++ pages/incidents.php
@@ -63,10 +63,8 @@
 					$publication = "USA Today";
 				}
 				else {  $str = file_get_contents($link2);
-                                        if(strlen($str)>0){
-                                           $str = trim(preg_replace('/\s+/', ' ', $str)); // supports line breaks inside <title>
-                                           preg_match("/\<title\>(.*)\<\/title\>/i",$str,$title); // ignore case
-                                           $publication= $title ;}
+                                        preg_match("/\<title\>(.*)\<\/title\>/i",$str,$title); 
+                                        $publication= $title ;}
 
 				}
 				// adding changes to remove ID when no case study exists
