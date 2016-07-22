988edb202d9075b4a06645d32e743b0a94d94113
eshasharma <eshasharma@users.noreply.github.com>
2016-07-22 15:09:58 -0400

    Update incidents.php

--- pages/incidents.php
+++ pages/incidents.php
@@ -66,7 +66,7 @@
 				        if(strlen($str)>0){
                                               $str = trim(preg_replace('/\s+/', ' ', $str)); // supports line breaks inside <title>
                                               preg_match("/\<title\>(.*)\<\/title\>/i",$str,$title); // ignore case
-                                              $publication= $title[1] ;
+                                              $publication= $title[1] ;}
                                           else
                                                $publication= "none" ;
 				
