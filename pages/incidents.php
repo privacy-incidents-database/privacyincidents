8612879e557465556e7571dfc0869133ecbca773
eshasharma <eshasharma@users.noreply.github.com>
2016-07-22 14:12:41 -0400

    Update incidents.php

--- pages/incidents.php
+++ pages/incidents.php
@@ -63,15 +63,15 @@
 					$publication = "USA Today";
 				}
 				else {  $str = file_get_contents($link2);
-				        //publication=$str ;
-				        $tg2 = implode("", $str); 
-				        $publication= $tg2 ;
+				       
+				        //$tg2 = implode("", $str); 
+				        $publication= $tg2[1] ;
 				        if(strlen($str)>0){
                                              $str = trim(preg_replace('/\s+/', ' ', $str)); // supports line breaks inside <title>
                                              preg_match("/\<title\>(.*)\<\/title\>/i",$str,$title); // ignore case
                                              $publication= $title[1] ;}
-                                        //else
-                                           ///  $publication= "none" ;
+                                        else
+                                             $publication= "none" ;
                                       
 
 				}
