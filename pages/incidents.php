ebe86c9859c6be89143a4cea437f78059df0355e
eshasharma <eshasharma@users.noreply.github.com>
2016-07-26 15:08:55 -0400

    Update incidents.php

--- pages/incidents.php
+++ pages/incidents.php
@@ -29,7 +29,7 @@
         
         $limit = 'LIMIT ' .$rows_per_page;
         echo $limit ; 
-	$incidents = mysql_query("SELECT `date_occurred`, `Descr`, `link`, `who_company`, `who_role`, `what_kind`, `Location`, `incident_root_cause`, `IncidentID`, `case study` FROM `Privacy incidents` $limit where review=1 ORDER BY date_occurred DESC");
+	$incidents = mysql_query("SELECT `date_occurred`, `Descr`, `link`, `who_company`, `who_role`, `what_kind`, `Location`, `incident_root_cause`, `IncidentID`, `case study` FROM `Privacy incidents` LIMIT 15 where review=1 ORDER BY date_occurred DESC");
 	if (mysql_num_rows($incidents) == 0) {
 		echo 'No incidents found!'; 
 		die();
