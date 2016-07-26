4330f74068ba81b6a6d71a9498934ddf633370b7
eshasharma <eshasharma@users.noreply.github.com>
2016-07-26 15:20:58 -0400

    Update incidents.php

--- pages/incidents.php
+++ pages/incidents.php
@@ -27,9 +27,8 @@
             $pageno = 1;
         } // if
         
-        $limit = 'LIMIT ' .$rows_per_page;
-        echo $limit ; 
-	$incidents = mysql_query("SELECT `date_occurred`, `Descr`, `link`, `who_company`, `who_role`, `what_kind`, `Location`, `incident_root_cause`, `IncidentID`, `case study` FROM `Privacy incidents` where review=1 ORDER BY date_occurred DESC $LIMIT");
+       
+	$incidents = mysql_query("SELECT `date_occurred`, `Descr`, `link`, `who_company`, `who_role`, `what_kind`, `Location`, `incident_root_cause`, `IncidentID`, `case study` FROM `Privacy incidents` where review=1 ORDER BY date_occurred DESC LIMIT $rows_per_page");
 	if (mysql_num_rows($incidents) == 0) {
 		echo 'No incidents found!'; 
 		die();
