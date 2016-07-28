e196d625f1be8fd20d67a533087fdba27af66e3f
eshasharma <eshasharma@users.noreply.github.com>
2016-07-27 22:08:33 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'pages/layout/header.html';?>
 
   <!-- Header Carousel -->
-  <header id="myCarousel" class="carousel slide" data-ride="carousel" data-interval=false pause=true>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval=false pause=true>
       <!-- Indicators -->
       <ol class="carousel-indicators">
           <li data-target="#myCarousel" data-slide-to="0"></li>
