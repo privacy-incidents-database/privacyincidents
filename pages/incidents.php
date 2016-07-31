dd0b7b9dd00cb44baf8e09bf81367759b00da894
eshasharma <eshasharma@users.noreply.github.com>
2016-07-31 18:26:20 -0400

    Update incidents.php

--- pages/incidents.php
+++ pages/incidents.php
@@ -139,9 +139,11 @@
 					
 				echo '<tr>';
 				echo '<td>' .$newDate. '</td>';
-				echo '<td><button>' .$tags2. '</button></td>'; 
-				//echo '<td><button> .$tag2. </button></td>' ;
-				//echo '<td>' .$tags2. '</td>';
+				//echo '<td><button>' .$tags2. '</button></td>'; 
+			        //'<td>'
+			        foreach ($newTags as &$tag1){
+				   echo '<td><button>' .$tags2. '</button><td>'; 
+				}
 				echo '<td>' . $descr. '</td>';
 				echo '<td><a href="' . $link . '" target=_blank>' . $publication. '</a></td>';
 				echo '<td><a href= "'. $caseStudy. '" target=_blank>'.$incidentID1. '</a></td>';
