41fa2c9eca95c5824b06e16f604dcbe570824a24
eshasharma <eshasharma@users.noreply.github.com>
2016-07-31 18:29:58 -0400

    Update incidents.php

--- pages/incidents.php
+++ pages/incidents.php
@@ -138,7 +138,7 @@
 			        foreach ($newTags as &$tag1){
 				  '<button>' .$tag1. '</button>'; 
 				}
-				<'/td'>
+			        '</td>'
 				echo '<td>' . $descr. '</td>';
 				echo '<td><a href="' . $link . '" target=_blank>' . $publication. '</a></td>';
 				echo '<td><a href= "'. $caseStudy. '" target=_blank>'.$incidentID1. '</a></td>';
