1fdea3db406305bb04110f94d61490066e8a0c3f
eshasharma <eshasharma@users.noreply.github.com>
2016-08-05 18:45:22 -0400

    Update incidents.php

--- pages/incidents.php
+++ pages/incidents.php
@@ -146,7 +146,7 @@
                                //    }
                                //array_shift($tags1) ;
 			        foreach ($tags1 as &$tag1){   
-			           $button_panel =$button_panel."  ".'<button id=$tag1 class=$tag1 onclick="changeColor(this,$tag1)">'.$tag1.'</button>'; 
+			           $button_panel =$button_panel."  ".'<button id=$tag1 class="me" onclick="changeColor(this,"me")">'.$tag1.'</button>'; 
 			            
 			       
   				}
