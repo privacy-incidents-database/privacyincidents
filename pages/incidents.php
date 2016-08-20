db8f5ca466bf8f1e097480a27b9fa7cdbc9e2ffe
eshasharma <eshasharma@users.noreply.github.com>
2016-08-20 17:18:45 -0400

    Update incidents.php

--- pages/incidents.php
+++ pages/incidents.php
@@ -46,7 +46,7 @@
 	<div>
 		<table class="table table-striped table-bordered">
 			<thead>
-				<tr>    <th class ="col-md-1">Date </th><td sortable="yes"></td>
+				<tr>    <th class ="col-md-1">Date </th>
 					<th class="tags col-md-4">Tags</th>
 					<th class="descr">Description</th>
 					<th class="resource col-md-1">Resource</th>
