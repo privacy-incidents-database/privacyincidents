defb607990d34bbeaf05f4a8f99c92363df4d87f
rtmarsha <rtmarsha@ncsu.edu>
2016-09-07 20:37:57 -0400

    Reverting changes to see which index.php worked

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
 		</div>
 -->
 		<div id="definitions">
-			<h3>What is a privacy incident?!</h3>
+			<h3>What is a privacy incident?</h3>
 In this research project, a privacy incident is:
 <ul>
 	<li>An instance of accidental or unauthorized collection, use or exposure of sensitive information</li>
