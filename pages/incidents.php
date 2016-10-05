196d55f87e039ca08a3f49e20cd6eeb98fc63da6
eshasharma <eshasharma@users.noreply.github.com>
2016-10-05 19:50:24 -0400

    Update incidents.php

--- pages/incidents.php
+++ pages/incidents.php
@@ -9,9 +9,9 @@ session_start();
         ?>
 
 <?php  
-        echo " <a href='{$_SERVER['PHP_SELF']}?show_all=' '>SHOW ALL INCIDENTS</a> "; 
-        
-        echo " <a href='{$_SERVER['PHP_SELF']}?show_few=' '>SHOW 15 INCIDENTS PER PAGE/a> ";       
+        echo " <a href='{$_SERVER['PHP_SELF']}?show_all=' ''>SHOW ALL INCIDENTS</a> "; 
+  
+        echo " <a href='{$_SERVER['PHP_SELF']}?show_few=' ''>SHOW 15 INCIDENTS PER PAGE/a> ";       
 ?>
 
 <?php
