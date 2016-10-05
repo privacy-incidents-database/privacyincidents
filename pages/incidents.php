eb359b9273329d18396f7c6967ce22dee7f1ef8c
eshasharma <eshasharma@users.noreply.github.com>
2016-10-05 15:54:54 -0400

    Update incidents.php

--- pages/incidents.php
+++ pages/incidents.php
@@ -189,8 +189,7 @@
                 echo " <a href='{$_SERVER['PHP_SELF']}?pageno=$nextpage'>NEXT</a> ";
                 echo " <a href='{$_SERVER['PHP_SELF']}?pageno=$lastpage'>LAST</a> ";
                  } 
-	        echo "<a href='{$_SERVER['PHP_SELF']}?$limit= 'LIMIT ' .($pageno - 1) * $numrows.',' .$numrows'>SHOW ALL</a>" ; 
-                 ?>
+	          ?>
         
        
 <?php include 'layout/footer.html';?>
