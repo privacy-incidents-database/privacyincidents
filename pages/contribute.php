e242ab931a2017d9f923e224618faa9e74d2a2f9
rtmarsha <rtmarsha@ncsu.edu>
2017-01-11 19:47:53 -0500

    Update contribute.php

--- pages/contribute.php
+++ pages/contribute.php
@@ -1,10 +1,4 @@
-<?php
-	include 'layout/header.html';
-	include 'db.php';
-
-	@mysql_connect(host,user,pw) or die('Could not connect to MySQL database. ');
-	mysql_select_db(database);
-
-?>
-<?php include 'layout/contribute.html';?>
-<?php include 'layout/footer.html';?>
+<html>
+   <h1>The Privacy Incidents Database has been moved to a new site.</h1>
+   <h2>Please click <a href="https://sites.google.com/site/privacyincidentsdatabase/?pli=1">here</a> to visit the new site!</h2>
+</html>
